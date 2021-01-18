939c2413730fb2547ecd3c3bb6375a0ca2771da6
samuel-coutinho <sfcrp2000@gmail.com>
2021-01-18 11:47:53 -0500

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :api do
+    namespace :v1 do
+      get 'avengers/index'
+      post 'avengers/create'
+      get '/show/:id', to: 'avengers#show'
+      delete '/destroy/:id', to: 'avengers#destroy'
+    end
+  end
+  root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
